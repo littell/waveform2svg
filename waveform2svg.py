ae8e5a8eba0408b39b452241b513ef508aa5a166
Littell <Patrick.Littell@ds.nrc.ca>
2019-04-09 23:31:07 -0400

    Render the SVG file

--- waveform2svg.py
+++ waveform2svg.py
@@ -3,58 +3,76 @@
 
 ###################################################
 #
-# add_ids_to_xml.py
+# waveform2svg.py
 #
-# In order to tell visualization systems, "highlight this
-# thing at this time", the document has to be able to identify
-# particular elements.  If the original document does NOT have
-# id tags on its elements, this module adds some.
-#
-# The auto-generated IDs have formats like "s0w2m1" meaning
-# "sentence 0, word 2, morpheme 1".  But it's flexible if some elements
-# already have ids, or if the markup uses different tags than a TEI document.
+# Make an SVG of a waveform, using roughly the same
+# procedure as Audacity uses: chop up the samples into
+# n buckets, plot only the maximum and minimum values.
 #
 ###################################################
 
 from __future__ import print_function, unicode_literals, division, absolute_import
 from io import open
-import logging, argparse
+import logging, argparse, os
+import numpy as np
 import soundfile as sf
 from math import ceil
-from svgwrite import Drawing
 import pystache
 
-SVG_TEMPLATE = '''<svg height="200" width="256">
-    <polygon points="{{#points}}{{x}},{{y}}{{/points}}">
+def ensure_dirs(path):
+    dirname = os.path.dirname(path)
+    if not dirname:
+        return
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+SVG_TEMPLATE = '''<svg height="{{height}}" width="{{width}}">
+    <polygon points="{{#points}}{{x}},{{y}} {{/points}}" />
 </svg>
 '''
 
-
-def make_svg(output_path, max_amplitudes, min_amplitudes, height=200, width=256):
-    data = { "points": [] }
-    for i, max_amp in enumerate(max_amplitudes):
-        x = (1.0 - max_amp) * (height / 2)
-        y = float(i)
-        data["points"].append({"x":x, "y":y})
-    return pystache.render(SVG_TEMPLATE, data)
-
-def go(input_path, output_path, num_buckets=256):
-    data, samplerate = sf.read('mouse1.wav', always_2d=True)
-    data = data.max(axis=1)
+def read_wav(input_path, num_buckets):
+    data, samplerate = sf.read(input_path, always_2d=True)
+    data = data.max(axis=1)  # max along each channel
+    data /= np.abs(data).max(axis=0)  # normalize
     num_samples = data.shape[0]
     samples_per_bucket = int(ceil(num_samples / num_buckets))
-    max_amplitudes = []
-    min_amplitudes = []
+    max_amps, min_amps = [], []
     for i in range(0, num_samples, samples_per_bucket):
-        max_amplitudes.append(data[i:i+256].max())
-        min_amplitudes.append(data[i:i+256].min())
+        max_amps.append(data[i:i+256].max())
+        min_amps.append(data[i:i+256].min())
+    return max_amps, min_amps
 
+def make_svg(input_path, num_buckets, include_negative=True, height=100, width=512):
+    max_amps, min_amps = read_wav(input_path, num_buckets)
+    data = { "height": height, "width": width, "points": [] }
+    data["points"].append({"x":0.0, "y": height / 2})
+    for i, max_amp in enumerate(max_amps):
+        y = (1.0 - max_amp) * (height / 2)
+        y = "%.2f" % y
+        x = i / num_buckets * width + 0.5
+        data["points"].append({"x":x, "y":y})
+    data["points"].append({"x":float(width), "y": height / 2})
+    if include_negative:
+        min_amps.reverse()
+        for i, min_amp in enumerate(min_amps):
+            y = (1.0 - min_amp) * (height / 2)
+            y = "%.2f" % y
+            x = width - (i / num_buckets * width + 0.5)
+            data["points"].append({"x":x, "y":y})
+    return pystache.render(SVG_TEMPLATE, data)
+
+def go(input_path, output_path, num_buckets=512, include_neg=True):
+    svg = make_svg(input_path, num_buckets, include_neg)
+    ensure_dirs(input_path)
+    with open(output_path, "w", encoding="utf-8") as fout:
+        fout.write(svg)
 
 if __name__ == '__main__':
      parser = argparse.ArgumentParser(description='Convert a WAV file to a SVG file of its waveform')
      parser.add_argument('input', type=str, help='Input WAV file')
      parser.add_argument('output', type=str, help='Output SVG file')
-
-     parser.add_argument('--nbuckets', type=int, default=256, help='Number of sample buckets (default: 256)')
+     parser.add_argument('--nbuckets', type=int, default=512, help='Number of sample buckets (default: 256)')
+     parser.add_argument('--include_neg', type=bool, default=True, help='Include negative values? (default: True')
      args = parser.parse_args()
-     go(args.input, args.output, args.nbuckets)
+     go(args.input, args.output, args.nbuckets, args.include_neg)
